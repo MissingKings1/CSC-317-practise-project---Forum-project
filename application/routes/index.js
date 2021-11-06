990a74075424ef0cbb905c4b1a75e857e5ada5a9
MissingKings1 <54790459+MissingKings1@users.noreply.github.com>
2021-11-05 21:17:09 -0700

    UPDATE

---
+++ application/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'CSC 317 App', name: "Yongjie He" });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.get('/registration', function(req, res, next) {
+    res.render('registration');
+});
+
+router.get('/postimage', function(req, res, next) {
+    res.render('postimage');
+});
+
+router.get('/viewpost', function(req, res, next) {
+    res.render('viewpost');
+});
+
+module.exports = router;
